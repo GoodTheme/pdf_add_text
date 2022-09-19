46b905b582f3657e5fd1b74bd84c56032df8a9a6
Alan Liu <goodthemel@gmail.com>
2022-09-19 17:39:14 +0800

    Update pdf_addtext.py

--- pdf_addtext.py
+++ pdf_addtext.py
@@ -160,11 +160,11 @@ class ui_main(QDialog, Ui_widget):
 		self.x_box.currentTextChanged.connect(self.x_change)
 		self.y_box.currentTextChanged.connect(self.y_change)
 
-		s = '在每页以(x, y)为文字框左上角座标添加文字，(0, 0)为页面左下角，(100, 100)为页面右上角；\n\n'\
+		s = '在每页以(x, y)为文字框左上角坐标添加文字，(0, 0)为页面左下角，(100, 100)为页面右上角；\n\n'\
 		'使用/A替代逐页递增的自然数，'\
 		'在/A后紧跟着的<>中使用/B、/C和数字分别表示按书签递增、中文数字和起始数字（优先级高），'\
 		'例如“第/A</C10>页”表示从“第十页”开始；\n\n'\
-		'可使用<br/>换行，<strike>删除线</strike>，<u>下划线</u><font color=red size=20>'\
+		'可使用<br/>换行，<strike>删除线</strike>，<u>下划线</u>，<font color=red size=20>'\
 		'文字颜色、大小</font>等XML标记，少部分英文字体可用'\
 		'<b>加粗</b>，<i>斜体</i>。'
 		self.input_string_view.setPlaceholderText(s)
